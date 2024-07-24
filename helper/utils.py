e2b57fa8492b48ee4d0317e7cf1ef1c2bcaaf2aa
Gunnugupta <166784443+Gunnugupta@users.noreply.github.com>
2024-07-24 23:58:50 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -87,7 +87,45 @@ async def not_subscribed(_, client, message):
       if user.status != enums.ChatMemberStatus.BANNED:                       
          return False 
    return True
-         
+         async def react_msg(client, message):
+    emojis = [
+        "👍",
+        "❤",
+        "🔥",
+        "🥰",
+        "👏",
+        "😁",
+        "🤔",
+        "😱",
+        "🎉",
+        "🤩",
+        "🤡",
+        "😍",
+        "❤‍🔥",
+        "🌚",
+        "🤣",
+        "⚡",
+        "🏆",
+        "🤨",
+        "😐",
+        "😈",
+        "🤓",
+        "👻",
+        "😇",
+        "🤝",
+        "🤗",
+        "🫡",
+        "🎅",
+        "🎄",
+        "🆒",
+        "😘",
+        "😎",
+    ]
+    rnd_emoji = random.choice(emojis)
+    await client.send_reaction(
+        chat_id=message.chat.id, message_id=message.id, emoji=rnd_emoji, big=True
+    )
+    return
 
 
 
